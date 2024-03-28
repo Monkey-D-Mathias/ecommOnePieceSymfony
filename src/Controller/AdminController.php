18f77b82fb879d7b2cd1bc5a78f6b543066cea95
Monkey-D-Mathias <mathiasabatord@gmail.com>
2024-03-28 16:02:36 +0100

    navBar and userAdmin crud

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,10 +7,12 @@ use App\Repository\CartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/', name: 'app_admin')]
     public function index(CartRepository $cartRepo): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
@@ -25,6 +27,14 @@ class AdminController extends AbstractController
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/users', name: 'app_user_index', methods: ['GET'])]
+    public function indexUsers(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/users/index.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,13 +17,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
+   
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
